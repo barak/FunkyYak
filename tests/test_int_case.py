b8500132def1f7df0daa9b8abb070ae9d49d1a4e
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2015-03-06 23:25:35 +0000

    Test that an int arg to grad is okay

---
+++ tests/test_int_case.py
@@ -0,0 +1,8 @@
+from test_util import *
+from funkyyak import grad
+
+def test_int_case():
+    check_equivalent((lambda x:x*x)(2.0),         4.0)
+    check_equivalent((lambda x:x*x)(2) + 0.0,     4.0)
+    check_equivalent(grad(lambda x:x*x)(2.0),     4.0)
+    check_equivalent(grad(lambda x:x*x)(2) + 0.0, 4.0)
