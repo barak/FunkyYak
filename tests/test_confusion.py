5921b722218c394652d895e106eb8131fefdc79e
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2015-03-07 09:12:52 +0000

    Oops, missed x×• in the confusion

--- tests/test_confusion.py
+++ tests/test_confusion.py
@@ -6,5 +6,5 @@ from funkyyak import grad
 ## 21(4):361-76, doi:10.1007/s10990-008-9037-1
 
 def test_nest_hosc():
-    shouldBeTwo = grad (lambda x: grad (lambda y: x*y) (2.0)) (1.0)
+    shouldBeTwo = grad (lambda x: x * grad (lambda y: x*y) (2.0)) (1.0)
     check_equivalent(shouldBeTwo, 2.0)
